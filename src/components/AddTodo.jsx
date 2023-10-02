f252c09a2d67891e1b66f3a59883bf44c03bba3a
Ikwukala-Mbas Ifechimenin <mbas750@gmail.com>
2023-10-02 17:19:11 +0100

    add error message

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -2,31 +2,44 @@ import { useState } from "react";
 
 const AddTodo = ({ addTodo }) => {
   const [text, setText] = useState("");
+  const [showError, setShowError] = useState({message: '', hasError: false})
 
   const handleAddClick = (e) => {
     e.preventDefault()
 
-    if (text.trim() !== "") {
+    if (text.trim() === "") {
+      setShowError({message: 'Please add todo!!!', hasError: true})
+
+    } else {
       addTodo(text);
       setText("");
-    } else {
-        alert('No todo added!!!')
+      setShowError({message: '', hasError: false})
     }
     
     // console.log(text)
   };
+
   return (
     <>
       <input
         type="text"
-        className=" text-black border-2 border-slate-700 p-3 w-full placeholder:text-grey-950 rounded-lg mb-5"
+        className=" text-black border-2 border-slate-700 p-3 w-full placeholder:text-grey-950 rounded-lg"
         placeholder="Add todo"
         value={text}
-        onChange={(e) => setText(e.target.value)}
+        onChange={(e) => {
+          setText(e.target.value)
+          setShowError({hasError: false})
+        }}
       />
+      {/* <ErrorMsg /> */}
+      
+      {showError.hasError && <div className="bg-red-700 w-64 rounded-lg animate-pulse mt-1">
+      {showError.message}
+     </div>}
+
       <button
         type="submit"
-        className="text-white mx-3 bg-red-700 rounded-lg p-3 mb-5 hover:bg-red-800"
+        className="text-white mx-3 bg-red-700 rounded-lg p-3 mb-5 hover:bg-red-800 mt-3"
         onClick={handleAddClick}
       >
         Add Todo
